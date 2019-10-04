921da0d8cad2078e86c6e85ccc3c0046865ac2e1
niv789 <46987197+niv789@users.noreply.github.com>
2019-10-04 20:07:02 +0530

    ADDED COMMENTS

--- algorithms/bubbleSort.py
+++ algorithms/bubbleSort.py
@@ -1,7 +1,13 @@
+# Python program for implementation of Bubble Sort
 def bubbleSort(arr):
     l = len(arr)
+    # Traverse through all array elements
     for i in range(l):
+         # Last i elements are already in place
         for j in range(0, l-i-1):
+            # traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
